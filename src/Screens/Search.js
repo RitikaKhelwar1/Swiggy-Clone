6ed250905b576564689da5e104cd0d33cd3187e4
admin <admin@admins-iMac.local>
2022-03-22 17:08:05 +0530

    minor changes

--- src/Screens/Search.js
+++ src/Screens/Search.js
@@ -32,26 +32,48 @@ function Search() {
   ]
   return (
    <View style={Styling.MainContainer}>
+     
+     {/* Search Bar in the Search Screen */}
+
      <View style={{borderColor:"black",borderWidth:1,margin:10}}>
      <TextInput placeholder='Seach for restaurant, item or more' style={{padding:15}}></TextInput>
      </View>
      <ScrollView showsVerticalScrollIndicator={false}>
+       
+       {/* Recent Searches */}
+
      <View style={{borderColor:"whitesmoke",borderBottomWidth:10,borderTopWidth:1.5 ,marginTop:10,paddingVertical:15}}>
-         <Text style={Styling.font}>Recent Searches</Text>
+         <Text style={[Styling.font,{marginBottom:15}]}>Recent Searches</Text>
         <FlatList data={SearchData}
         renderItem={(element)=>{
           return(
-            <View style={{flex:1,flexDirection:"row",borderColor:"black",borderWidth:1,borderRadius:20,margin:10,padding:5}}>
+            <View style={{flex:1,flexDirection:"row",borderColor:"grey",borderWidth:1,borderRadius:20,margin:5,padding:5}}>
               <Image source={require('../Images/wall-clock.png')} style={Styling.headerLogo} ></Image>
-              <Text>{element.item.search}</Text>
+              <Text  style={{marginLeft:10,fontSize:12.5}}>{element.item.search}</Text>
             </View>
           )
         }}
         numColumns={3}
-        >
-
-        </FlatList>
+        > </FlatList>
          </View>
+
+        {/* Trending Searches */}
+
+        <View style={{borderColor:"whitesmoke",borderBottomWidth:10,borderTopWidth:1.5 ,marginTop:10,paddingVertical:15}}>
+         <Text style={[Styling.font,{marginBottom:15}]}>Trending Searches</Text>
+        <FlatList data={SearchData}
+        renderItem={(element)=>{
+          return(
+            <View style={{flex:1,flexDirection:"row",borderColor:"grey",borderWidth:1,borderRadius:20,margin:5,padding:5}}>
+              <Image source={require('../Images/trend.png')} style={Styling.headerLogo} ></Image>
+              <Text style={{marginLeft:5,fontSize:12.5}} >{element.item.search}</Text>
+            </View>
+          )
+        }}
+        numColumns={3}
+        > </FlatList>
+         </View>
+
      </ScrollView>
      </View>
   )
