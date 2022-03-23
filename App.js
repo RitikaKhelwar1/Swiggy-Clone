b1461534312a5b33e54facc4bc53bc445bcdc4c4
admin <admin@admins-iMac.local>
2022-03-23 18:54:57 +0530

    minor changes

--- App.js
+++ App.js
@@ -1,31 +1,17 @@
 import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+ 
 } from 'react-native';
-import Component1 from './src/Screens/component1';
 import Styling from './src/CSS/Styling';
-import Component2 from './src/Screens/Component2';
-import Header from './src/Screens/header'
-import Component3 from './src/Screens/Component3';
-import SubComponent3 from './src/Screens/SubComponent3';
-import PopularCurations from './src/Screens/PopularCurations'
-import PopularBrands from './src/Screens/PopularBrands';
-import Instamart from './src/Screens/Instamart';
 import Footer from './src/Screens/Footer';
 import { NavigationContainer } from '@react-navigation/native';
-import UserProfile from './src/Screens/UserProfile';
 
 const App = () => {
   return (
 
     <SafeAreaView style={Styling.MainContainer} >
-      
+      {/* <----------Bottom Tabs-----------> */}
       <NavigationContainer>
       <Footer/>
       </NavigationContainer>
